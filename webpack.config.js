547d6a95ddc915ff02cd63db75eec8216cf6a5e6
Michael Sullivan <sullivanmichael344@gmail.com>
2024-09-29 21:53:06 -0400

    Make file template

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: "eval-source-map",
   plugins: [
     new HtmlWebpackPlugin({
         filename: 'index.html',
